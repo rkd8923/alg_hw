b1480f0ad87bf858f36d85587d432f5ae80b264e
Jiwon Kang <rkd8923@gmail.com>
2018-05-15 14:20:44 +0900

    m2

--- hw2/test.py
+++ hw2/test.py
@@ -1,6 +1,14 @@
 import sys
 o = open("input3.txt", "w")
-
+o.write("I " + str(999) + '\n')
+o.write("S " + str(999) + '\n')
+o.write("S " + str(1) + '\n')
+o.write("D " + str(999) + '\n')
+o.write("R " + str(1) + '\n')
+for x in range(1, 10):
+    o.write("S " + str(x) + '\n')
+    o.write("D " + str(x) + '\n')
+    o.write("R " + str(x) + '\n')
 for x in range(1, 1000):
     o.write("I " + str(x) + '\n')
 for x in range(1, 1000):
