982ee8318f7974f9949d52c03b304b03ae232994
Jiwon Kang <rkd8923@gmail.com>
2018-05-31 14:58:16 +0900

    add dense

---
+++ hw3/test/make_test.py
@@ -0,0 +1,23 @@
+import sys
+import random
+
+n = 3000
+
+f = open("dense3000.txt", 'w')
+f.write(str(n) + '\n')
+
+for x in range(1, n+1):
+    tmp = list(range(1, n+1))
+    tmp.remove(x)
+    r = len(tmp)
+    tmp.insert(0, r)
+    # while (len(tmp) != r+1):
+        # t = random.randint(1, n)
+        # if (t == x) or (t in tmp[1:]):
+            # continue
+        # tmp.append(t)
+    for y in tmp:
+        f.write(str(y) + ' ')
+    f.write('\n')
+
+f.close()
