bfb4e49c44c81ecce8c1e24d04b6f933cae41600
Jiwon Kang <rkd8923@gmail.com>
2018-05-31 14:25:37 +0900

    test file add

---
+++ hw3/test/make_file.py
@@ -0,0 +1,22 @@
+import sys
+import random
+
+n = 3000
+
+f = open("sparse3000.txt", 'w')
+f.write(str(n) + '\n')
+
+for x in range(1, n+1):
+    r = random.randint(0, 2)
+    tmp = []
+    tmp.append(r)
+    while (len(tmp) != r+1):
+        t = random.randint(1, n)
+        if (t == x) or (t in tmp[1:]):
+            continue
+        tmp.append(t)
+    for y in tmp:
+        f.write(str(y) + ' ')
+    f.write('\n')
+
+f.close()
